88c51645a823bb238bafc2da611724f1ac144a3c
matidev-campos <matidev.campos@gmail.com>
2026-01-26 11:52:45 -0300

    feature: vehicle rental persist using localStorage

--- src/pages/VehicleDetailPage.tsx
+++ src/pages/VehicleDetailPage.tsx
@@ -1,4 +1,7 @@
-import { getVehicleById } from "@/api/vehicles.service";
+import { getVehicleById, isVehicleAvailable, rentVehicle } from "@/api/vehicles.service";
+import VehicleInfo from "@/components/vehicle/VehicleInfo";
+import type { Vehicle } from "@/types";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 
@@ -11,18 +14,37 @@ export default function VehicleDetailPage(){
     }
 
     const vehicleId = Number(id);
-    const vehicle = getVehicleById(vehicleId);
+    const initialVehicle = getVehicleById(vehicleId);
+
+    const [vehicle, setVehicle] = useState<Vehicle | null>(initialVehicle ?? null);
 
     if(!vehicle){
         return <p>Vehicle not found</p>
     }
 
+    function handleRent(){
+        if(!vehicle) return;
+        
+        try{
+            const rendedVehicle = rentVehicle(vehicle);
+            setVehicle(rendedVehicle);
+        }catch(error){
+            alert((error as Error).message);
+        }
+        
+    }
+
+    const available = isVehicleAvailable(vehicle);
+
     return(
     <div>
-            <h3>{vehicle.brand} {vehicle.model}</h3>
-            <p>Year: {vehicle.year}</p>
-            <p>Price per day: ${vehicle.pricePerDay}</p> 
-            <p>Status: {vehicle.status}</p>
+            <VehicleInfo vehicle={vehicle} />
+
+            {available ? (
+                <button onClick={handleRent}>Rent vehicle</button>
+            ) : (
+                <p>this vehicle is not available</p>
+            )}
         </div>
     );
 
